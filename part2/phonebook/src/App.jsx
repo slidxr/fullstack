ebfc0b27952fe9b157e948ce53ded03697ee10d9
Eugen <47353881+slidxr@users.noreply.github.com>
2023-10-16 18:54:13 +0300

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const Filter = ({ filter, handleFilterChange }) => {
     return (
@@ -36,11 +37,17 @@ const Persons = ({ personsToShow }) => {
 }
 
 const App = () => {
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, []);
+
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+
     ])
 
     const [newName, setNewName] = useState('')
@@ -74,7 +81,7 @@ const App = () => {
         ? persons
         : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
-    
+
     return (
         <div>
             <h2>Phonebook</h2>
