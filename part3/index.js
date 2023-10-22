b963afdff4dc18c5dedca86eac4f60a6fe56f6b4
slidxr <47353881+slidxr@users.noreply.github.com>
2023-10-22 20:53:44 +0300

    Exercises 3.13-3.14

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const {request} = require("express");
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
+
 
 app.use(express.static('dist'))
 app.use(cors())
@@ -10,116 +13,64 @@ morgan.token('body', function (request) { return JSON.stringify(request.body) })
 app.use(express.json())
 app.use(morgan('tiny'))
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
-// Using express
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(result => {
+        response.json(result)
+    })
 })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people <br/> ${new Date()}`)
+    response.send(`Phonebook has info for ${Person.length} people <br/> ${new Date()}`)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find( person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.statusMessage = `Person with id ${id} not found`
-        response.status(404).end()
-    }
+    Person
+        .findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            response.status(400).send({error: 'malformatted id'})
+        })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    persons = persons.filter( person => person.id !== id)
+    person = Person.filter( person => person.id !== id)
     response.status(204).end()
 })
 
 const postMorgan = morgan(':method :url :status :res[content-length] - :response-time ms :body')
 
-app.post('/api/persons', postMorgan, (request, response) => {
+app.post('/api/persons', postMorgan, (request, response, next) => {
     const body = request.body
-    const id = Math.floor(Math.random() * 100)
 
-    if (!body.name) {
+    if(!body.name || !body.number){
         return response.status(400).json({
-            error: 'name missing'
+            error: 'name or number missing'
         })
-    }
-
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
+    }else{
+        const person = new Person({
+            name: body.name,
+            number: body.number,
         })
-    }
 
-    if (persons.find( person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
+        person.save()
+            .then(person => {
+                response.json(person)
+            })
+            .catch(error => next(error))
     }
-
-    const person = {
-        id: id,
-        name: body.name,
-        number: body.number
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
-
-// using clear node.js
-// const app = http.createServer((request, response) => {
-//     if (request.method === 'GET') {
-//         if (request.url === '/api/persons') {
-//             response.writeHead(200, { 'Content-Type': 'application/json' })
-//             response.end(JSON.stringify(persons))
-//         }
-//         else if (request.url === '/info') {
-//             response.writeHead(200, { 'Content-Type': 'application/json' })
-//             response.end(`Phonebook has info for ${persons.length} people \n${new Date()}`)
-//         }
-//     }
-//
-//
-// })
-//
-// const PORT = 3001
-// app.listen(PORT)
-// console.log(`Server running on port ${PORT}`)
-
 
