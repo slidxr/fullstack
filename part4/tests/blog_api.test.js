5397e567fb5b9b21572d71ab80b5c48316218636
slidxr <47353881+slidxr@users.noreply.github.com>
2023-10-25 19:43:26 +0300

    Exercise 4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+        title: 'Test Blog 1',
+    },
+    {
+        title: 'Test Blog 2',
+    }];
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let noteObject = new Blog(initialBlogs[0])
+    await noteObject.save()
+    noteObject = new Blog(initialBlogs[1])
+    await noteObject.save()})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(2)
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+})
