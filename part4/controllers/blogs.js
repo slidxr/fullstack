219a7d6e6dfc7ec85dd68d8063ff6dc00ee241a3
slidxr <47353881+slidxr@users.noreply.github.com>
2023-10-26 18:34:07 +0300

    Exercises 4.9-4.12

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -9,6 +9,12 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
+    if (!body.title || !body.url) {
+        return response.status(400).json({
+            error: 'title or url missing'
+        })
+    }
+
     const blog = new Blog({
         title: body.title,
         author: body.author,
@@ -18,6 +24,7 @@ blogsRouter.post('/', async (request, response, next) => {
 
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog)
+
 })
 
 blogsRouter.get('/:id', async (request, response, next) => {
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -37,6 +37,54 @@ test('there are two blogs', async () => {
     expect(response.body).toHaveLength(2)
 })
 
+test('unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].id).toBeDefined()
+})
+
+test('HTTP POST request to the /api/blogs URL successfully creates a new blog post', async () => {
+
+    const newBlog = {
+        title: 'Test Blog 3',
+        author: 'Test Author 3',
+        url: 'Test URL 3',
+        likes: 3
+    }
+
+    const test_post = await api.post('/api/blogs').send(newBlog)
+    expect(test_post.status).toBe(201)
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+
+})
+
+test('if the likes property is missing from the request, it will default to the value 0', async () => {
+
+        const newBlog = {
+            title: 'Test Blog 4',
+            author: 'Test Author 4',
+            url: 'Test URL 4'
+        }
+
+        const test_post = await api.post('/api/blogs').send(newBlog)
+        expect(test_post.status).toBe(201)
+        const response = await api.get('/api/blogs')
+        expect(response.body[response.body.length - 1].likes).toBe(0)
+
+})
+
+test('if the title or url properties are missing from the request data, ' +
+    'the backend responds to the request with the status code 400 Bad Request.', async () => {
+
+            const newBlog = {
+                author: 'Test Author 5',
+            }
+
+            const test_post = await api.post('/api/blogs').send(newBlog)
+            expect(test_post.status).toBe(400)
+    })
+
 afterAll(async () => {
     await mongoose.connection.close()
 })
