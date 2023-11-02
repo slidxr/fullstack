53429fce6a58ccd7adca246b30917b7c3a211d08
slidxr <47353881+slidxr@users.noreply.github.com>
2023-11-02 21:14:25 +0200

    Exercise 5.2

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -15,6 +15,14 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }}, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('loggin in with', username, password)
@@ -23,6 +31,11 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+          'loggedBlogappUser',
+          JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -60,10 +73,18 @@ const App = () => {
     )
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
   return (
       <div>
         <h2>blogs</h2>
-        <p> {user.name} logged in </p>
+        <p>
+          {user.name} logged in
+          <button onClick={handleLogout}> logout </button>
+        </p>
         {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
         )}
