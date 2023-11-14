90ccd76fdabe49e246b2ae5b293d489c03723361
slidxr <47353881+slidxr@users.noreply.github.com>
2023-11-14 14:44:22 +0000

    add eslint config

---
+++ part5/eslintrc.cjs
@@ -0,0 +1,54 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+    'plugins': [
+        'react', 'jest'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        'no-console': 0,
+        'react/prop-types': 0,
+        'react/react-in-jsx-scope': 'off'
+    },
+    'settings': {
+        'react': {
+            'version': 'detect'
+        }
+    },
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+}
